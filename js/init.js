d6df5f86135577a146a78cfaf6319ccd724979a9
shannonbit <emmanuelle.laguna@sparklabs.com.mx>
2016-07-30 20:42:38 -0500

    se agrego funcionalidad para extraer todos los reportes, un reporte en especifico y guardar imagen de persona desaparecida

--- js/init.js
+++ js/init.js
@@ -1,3 +1,12 @@
+var image = null;
+var config = {
+  apiKey: "AIzaSyCsMIcbhUR3H9PezGBEz1otPYh-tAAtlM0",
+  authDomain: "regresapi.firebaseapp.com",
+  databaseURL: "https://regresapi.firebaseio.com",
+  storageBucket: "regresapi.appspot.com",
+};
+firebase.initializeApp(config);
+
 (function($){
   $(function(){
 
@@ -11,7 +20,7 @@
 // genera ID unico
 function generateUUID() {
    var d = new Date().getTime();
-   var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+   var uuid = 'xxshxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
        var r = (d + Math.random()*16)%16 | 0;
        d = Math.floor(d/16);
        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
@@ -21,14 +30,52 @@ function generateUUID() {
 
 function dataParser() {
 var data = $("#reporte").serialize();
-var parseado = JSON.parse('{"' + decodeURI(data).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
-writeUserData(parseado,generateUUID());
- 
+data += '&imagen=' + image;
+var parsed_data = JSON.parse('{"' + decodeURI(data).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
+writeUserData(parsed_data,generateUUID());
+
 }
+
+function readImage(inputElement) {
+    var deferred = $.Deferred();
+
+    var files = inputElement.get(0).files;
+    if (files && files[0]) {
+        var fr= new FileReader();
+        fr.onload = function(e) {
+            deferred.resolve(e.target.result);
+        };
+        fr.readAsDataURL( files[0] );
+    } else {
+        deferred.resolve(undefined);
+    }
+
+    return deferred.promise();
+}
+
+$('#inp').on('change',function(){
+  readImage($(this)).done(function(base64Data){ image = base64Data; });
+});
 
 
 function writeUserData(data, generateUUID) {
   firebase.database().ref('report/' + generateUUID).set(data);
 }
 
+function getReportById(id){
+  firebase.database().ref('report/' + id).on('value', function(snapshot) {
+    var snap = snapshot.val();
+    var nombre = snap.nombre;
+    nombre = nombre.replace(/\+/g, ' ');
+    alert(nombre);
+    //$('#userName').html(nombre);
+  });
+}
 
+function getReport(id) {
+  firebase.database().ref('report/').on('value', function(snapshot) {
+    var snap = snapshot.val();
+    console.log(snap);
+    getLocation();
+  });
+}
